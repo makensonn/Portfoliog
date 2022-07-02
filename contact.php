1cfbefed8f0cdb9c69d3be2c71e58012cff0636b
makensonn <Makenson.noel@yahoo.com>
2022-07-02 00:30:00 +0000

    3rd commit

--- contact.php
+++
@@ -1,6 +0,0 @@
-$email=$_POST['email'];
-$message=json_encode($_POST);
-$receiver="makenson.noel@yahoo.com";
-$mailer="mailer@myservice.com";
-
-mail($email,"Message for enquiry or whatever",$message,  array("from"=>$mailer));
